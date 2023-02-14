70a57353897705fa4f0beaccc57c05a0c6e4f9c5
Halid Ali <halidziyaali@gmail.com>
2023-02-14 23:09:03 +0100

    Get products via service and display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nestjs-demo-ui';
+  products!: Product[];
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe((res) => {
+      this.products = res;
+    });
+  }
 }
